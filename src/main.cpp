19c5a897b94b836ab0ca450f2c95d4687f281fd9
Artyom Kolpakov <ddvamp007@gmail.com>
2025-01-14 02:13:08 +0300

    Better

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <config.hpp>
+
 #include <liba/src/liba.hpp>
 
 #include <format>
 #include <iostream>
 
 int main(int argc, char **argv) {
-  std::cout << argv[0] << " version " << EXAMPLE_VERSION_MAJOR << '.'
-            << EXAMPLE_VERSION_MINOR << '.' << EXAMPLE_VERSION_PATCH
-            << std::endl;
+  std::cout << argv[0] << " version " << version::kMajor << '.'
+            << version::kMinor << '.' << version::kPatch << '\n';
 
-  std::cout << std::format("Hello, world#{}!", FooA()) << std::endl;
+  std::cout << std::format("Hello, world #{}!", FooA()) << std::endl;
 
   return 0;
 }
